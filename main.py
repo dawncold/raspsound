e4cb534a167de59c6ed08edf44e8c301c11ad92a
dawncold <loooseleaves@gmail.com>
2016-02-19 21:28:38 +0800

    init

---
+++ main.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function, division
+import logging
+import tornado.ioloop
+import tornado.web
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        signature = self.get_query_argument('signature')
+        timestamp = self.get_query_argument('timestamp')
+        nonce = self.get_query_argument('nonce')
+        echo_str = self.get_query_argument('echostr')
+        LOGGER.info(timestamp)
+        LOGGER.info(nonce)
+        LOGGER.info(nonce)
+        self.write(echo_str)
+
+
+def make_app():
+    return tornado.web.Application([
+        (r"/wx", MainHandler),
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(5000)
+    tornado.ioloop.IOLoop.current().start()
